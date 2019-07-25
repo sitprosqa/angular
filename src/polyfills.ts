0d13868aec43c594b0af0c9da57e3bbd138f19e6
sitpros <sitprosqa@gmail.com>
2019-07-25 16:29:14 +0500

    nb

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -56,19 +56,9 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/number';
-import 'core-js/es6/math';
-import 'core-js/es6/string';
-import 'core-js/es6/date';
-import 'core-js/es6/array';
-import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
+
+
+
 import 'core-js/es6/array';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
@@ -78,9 +68,7 @@ import 'core-js/es6/array';
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
-/** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
@@ -96,5 +84,4 @@ import 'core-js/es7/reflect';
 /**
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
- */
-import 'intl';  // Run `npm install --save intl`.
+ */// Run `npm install --save intl`.
